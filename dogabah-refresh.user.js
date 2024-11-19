2c4162e1316cf87a4207faa6433b49c08acd80e0
Ray Yano | あねてあ <yanorei@hotmail.co.jp>
2024-11-19 14:42:23 +0900

    Update dogabah-refresh.user.js
    Co-authored-by: Ry0taK <49341894+Ry0taK@users.noreply.github.com>

--- dogabah-refresh.user.js
+++ dogabah-refresh.user.js
@@ -8,29 +8,16 @@
 // @updateURL    https://github.com/yanorei32/dogabah-refresh/raw/master/dogabah-refresh.user.js
 // @downloadURL  https://github.com/yanorei32/dogabah-refresh/raw/master/dogabah-refresh.user.js
 // @include      https://dagobah.net/flash/*
-// @version      0.1.1
+// @version      0.2.0
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=dagobah.net
 // @grant        none
 // @license      BSD-2-Clause
-// @run-at       document-end
+// @run-at       document-body
 // ==/UserScript==
 
 (function() {
-    const PROTECT_SIGNATURE = 'PROTECT!';
-
+    document.querySelectorAll('script').forEach(e => e.remove());
     const e = document.createElement('script');
     e.src = 'https://cdn.jsdelivr.net/npm/@ruffle-rs/ruffle/ruffle.min.js';
-    e.id = PROTECT_SIGNATURE;
-
     document.body.appendChild(e);
-
-    function cleanup() {
-        document.querySelectorAll('script')
-            .filter(e => e.id != PROTECT_SIGNATURE)
-            .forEach(e => e.remove());
-    }
-
-    cleanup();
-    document.addEventListener('DOMContentLoaded', cleanup);
-    document.addEventListener('load', cleanup);
 })();
